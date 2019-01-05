e6c2ba67d1c28512f511824cf5d23df3d58306d1
Warrick Ball <warrickball@gmail.com>
2019-01-05 19:59:32 +0000

    Gave check.py an option for the number of simulated power spectra over which to average (was hardcoded at 100)

--- examples/basic/check.py
+++ examples/basic/check.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as pl
 import AADG3
 from astropy.stats import LombScargle
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-N', type=int, default=100,
+                    help="number of power spectra to average (default=100)")
+args = parser.parse_args()
 
 def LS(t, y):
     f, p = LombScargle(t, y).autopower(normalization='psd',
@@ -13,13 +19,11 @@ def LS(t, y):
     p = p*np.mean(y**2)/np.sum(p)/(f[1]-f[0])  # Bill's normalization
     return f, p
 
-N = 100
-
 nml, modes, rot = AADG3.load_all_input('basic.in')
 
 y0 = np.loadtxt(nml['output_filename'])
 t0 = np.arange(len(y0), dtype=float)*nml['cadence']
-y = y0.reshape((N, -1))
+y = y0.reshape((args.N, -1))
 t = t0[:len(y[0])]
 
 # f, p_tot = LS(t0, y0)
@@ -30,7 +34,7 @@ f, p_tot = LS(t, y[0])
 for yi in y[1:]:
     p_tot += LS(t, yi)[1]
 
-pl.loglog(f, p_tot/N)
+pl.loglog(f, p_tot/args.N)
 ff = np.linspace(f[0], f[-1], 10000)
 pl.loglog(ff, AADG3.PS_model(ff, nml, modes, rot))
 pl.show()
